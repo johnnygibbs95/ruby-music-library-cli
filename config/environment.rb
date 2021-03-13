7bb6588dd4d5db62d55a8b6bbb614c48128a0b8c
johnnygibbs95 <johnnydgibbs1995@gmail.com>
2021-03-12 18:33:13 -0800

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,12 +2,12 @@ require 'bundler'
 Bundler.require
 require 'pry'
 
-# module Concerns
-#     module Findable
-#         def find_by_name(name)
-#             self.all.detect {|song| song.name == name}
-#         end
-#     end
-# end
+module Concerns
+    module Findable
+        def find_by_name(name)
+            self.all.detect {|song| song.name == name}
+        end
+    end
+end
 
 require_all 'lib'
--- lib/concerns/findable.rb
+++
@@ -1,7 +0,0 @@
-module Concerns
-    module Findable
-        def find_by_name(name)
-            self.all.detect {|song| song.name == name}
-        end
-    end
-end
