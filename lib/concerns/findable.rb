e91e53c674b6274141d6f0d5945b4f67a539f83a
johnnygibbs95 <johnnydgibbs1995@gmail.com>
2021-03-12 18:52:56 -0800

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,8 @@
 module Concerns
     module Findable
         def find_by_name(name)
-            self.all.detect {|song| song.name == name}
-            binding.pry
+            self.all.detect {|song| song.name == self}
+            #binding.pry
         end
     end
 end
